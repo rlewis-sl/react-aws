b1cb847c198c6d0ec283c5d2a0fcffc14a6bb17d
Robert Lewis <rablewis@algo-pop.com>
2021-11-10 15:38:21 +0000

    Factor out create-web script - also add iam_policy.json to show aws permissions needed for scripts

---
+++ aws/create-web.bat
@@ -0,0 +1,4 @@
+aws s3 mb s3://algo-pop-react-test
+aws s3api put-bucket-policy --bucket algo-pop-react-test --policy "{\"Version\": \"2012-10-17\", \"Statement\": [{\"Sid\": \"PublicReadGetObject\", \"Effect\": \"Allow\", \"Principal\": \"*\", \"Action\": \"s3:GetObject\", \"Resource\": \"arn:aws:s3:::algo-pop-react-test/*\"}]}"
+
+aws s3 website s3://algo-pop-react-test --index-document index.html --error-document index.html
